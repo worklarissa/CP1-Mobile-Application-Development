28f1e5af4b0094433b5e9ec86c023ba9ec11e4e6
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-09 10:32:50 -0300

    first commit

---
+++ CP1-Mobile-Application-Development/app/src/main/java/com/example/cp1_mobile_application_development/TelaTres.kt
@@ -0,0 +1,20 @@
+package com.example.cp1_mobile_application_development
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TelaTres : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_tres)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
